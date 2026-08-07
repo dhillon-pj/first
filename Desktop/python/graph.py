5e66663f622335aa5f1983727d708b2b15cc19c9
dhillon-pj <pd9538267@gmail.com>
2026-08-07 10:21:46 +0530

    python lib

---
+++ Desktop/python/graph.py
@@ -0,0 +1,3 @@
+import matplotlib  as plt
+plt.plot([1,2,3,4,5], [2,4,6,8,10])
+plt.show()
---
+++ Desktop/python/pr.py
@@ -0,0 +1,464 @@
+# import numpy as np
+# arr = np.array([10,20,30])
+# print(arr)
+
+# import numpy as np
+# arr = np.arange(1, 11)
+# print(arr)
+
+# import numpy as np
+# arr = np.arange(2, 21, 2)
+# print(arr)
+
+# import numpy as np
+# arr = np.array([10,20,30])
+# print(arr.dtype)
+
+# arr = np.array([5,10,15,20,25])
+# print(arr.size)
+
+# arr = np.array([[1,2,3],
+#                [4,5,6]])
+# print(arr)
+
+# arr = np.zeros(5)
+# print(arr)
+
+# import numpy as np
+# arr = np.array([[1,2,3], [4,5,6]])
+# print(arr.shape)
+
+# import numpy as np
+# arr = np.arange(1,13)
+# new_arr = arr.reshape(3,4)
+# print(new_arr)
+
+# import numpy as np
+# arr = np.arange(1,13)
+# print(arr.reshape(5.3))
+
+# import numpy as np
+# arr = np.array([1,2,3,4,5,6,])
+# new_arr = arr.reshape(2,3)
+# print(new_arr)
+
+# import numpy as np
+# arr = np.arange(1,9)
+# print(arr.reshape(4,2))
+
+# import numpy as np
+# arr = np.arange(1,13)
+# print(arr.shape)
+# new_arr = arr.reshape(3,4)
+# print(new_arr.shape)
+
+
+# import numpy as np
+# arr1 = np.array([1,2,3])
+# arr2 = np.array([4,5,6])
+# result = np.concatenate((arr1,arr2))
+# print(result)
+
+# import numpy as np
+# a = np.array([[1,2],[3,4]])
+# b = np.array([[5,6],[7,8]])
+# print(np.vstack((a,b)))
+
+# for i in range(1,6):
+#     print("*" * i)
+
+# for i in range(5,0,-1):
+#     print("*" * i)
+
+# for i in range(1,6):
+#     for j in range(1,i+1):
+#         print(j, end=" ")
+#     print()
+
+# for i in range(1,6):
+#     print(str(i) * i)
+
+# for i in range(1,6):
+#     print(" "*(5-i) + "*"*(2*i-1))
+
+# for i in range(5,0,-1):
+#     print (" "*(5-i) + "*"*i)
+
+# for i in range(1,6):
+#     print(" "*(5-i) + "*"*(2*i-1))
+
+# for i in range(4,0,-1):
+#     print(" "*(5-i) +"*"*(2*i-1))    
+
+
+# import pandas as pd
+# data = [100,200,104]
+# series = pd.Series(data)
+# print(series.to_string(index=False))
+
+# import pandas as pd
+# data =([5,10,15,20])
+# series = pd.Series(data)
+# print (series)
+
+# import pandas as pd 
+# data = (1,2,3,4)
+# series=pd.Series(data)
+# print(series.iloc[2])
+
+
+
+# import pandas as pd
+# data = {
+#     "A": 10,
+#     "B": 20,
+#     "C": 30
+# }
+# series =pd.Series(data)
+# print(series.loc["B"])
+
+
+# import pandas as pd
+# data=[10,20,30,40]
+# series=pd.Series(data,
+#                  index=["a","b","c","d"])
+# print(series[series>=20])
+
+
+# import pandas as pd
+# data={
+#     "A":10,
+#     "B":20,
+#     "C":30
+# }
+# series = pd.Series(data)
+# print(series[series("C")])
+
+
+# import pandas as pd
+# data = ([5,10,15,20])
+# series = pd.Series(data)
+# print(series)
+
+# import pandas as pd
+# data = (1,2,3,4)
+# series = pd.Series(data)
+# print(series.loc[2])
+
+# import pandas as pd
+# data ={
+#     "A": 10,
+#     "B": 20,
+#     "C": 30
+# }
+# series = pd.Series(data)
+# print(series.loc["B"])
+
+# import pandas as pd
+# data = [100,200,300,400]
+# series = pd.Series(data)
+# print(series.to_string(index =False))
+
+# import pandas as pd
+# data = [100,200,300]
+# series = pd.Series(data,
+#                 index = [1,2,3])
+# print(series)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
